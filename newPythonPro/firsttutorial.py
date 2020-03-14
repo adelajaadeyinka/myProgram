d3c639f1017dc01e71df5509722d142115f3feb6
adelajaadeyinka <adelajaadeyinka@gmail.com>
2020-03-14 11:18:08 +0000

    next commit

---
+++ newPythonPro/firsttutorial.py
@@ -0,0 +1,18 @@
+
+def c(firstname , lastname):
+     
+     return(firstname + lastname)
+     
+print( c(2, 3))
+
+
+def f(x):
+    return(x**2 + 2*x + 1)
+print(f(c(2,3)))
+print(c(f(2),f(2)))
+ 
+    
+    
+    
+    
+
